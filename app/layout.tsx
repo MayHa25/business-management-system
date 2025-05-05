2e073213e040a44d970d970eee2fdfedf20e4a1c
MayHa25 <may.hakimi010@gmail.com>
2025-05-05 16:05:31 +0300

    הוספת מערכת התראות FCM

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Rubik } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 import MainLayout from '@/components/layouts/main-layout';
-import FCMNotifications from '@/components/ui/fcm-notifications'; // ✅ הוספה כאן
+import FCMNotifications from '@/components/fcm-notifications'; // ✅ הוספה כאן
 
 const rubik = Rubik({ 
   subsets: ['hebrew', 'latin'],
